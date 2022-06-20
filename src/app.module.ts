ba6cd8d0318eaa122acf84abd1eda9988bbfbb73
Allan Gadelha <allangadelha@gmail.com>
2022-06-20 19:31:15 -0300

    Creating CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://localhost:27017/api-crud-nest'
+    ),
+    UsersModule
+  ],
   controllers: [],
   providers: [],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
+import { User, UserSchema } from './entities/user.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [UsersController],
   providers: [UsersService]
 })
