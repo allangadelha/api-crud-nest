d42c2fd6739cde28c041f161051c81e525ca12cc
Allan Gadelha <allangadelha@gmail.com>
2022-06-21 11:45:11 -0300

    Database in cluster

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb://localhost:27017/api-crud-nest'
+      'mongodb+srv://allandev:12345seis@cluster0.aquzq.mongodb.net/test'
     ),
     UsersModule
   ],
